5618dc71142f3b499b3d273ca8b35b12d472913c
tannazma <97254921+tannazma@users.noreply.github.com>
2023-01-03 16:01:48 +0100

    make massage box and chat platform

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
     console.log(req.url)
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("Hello")
+fetch ("http://127.0.0.1:3000/ardjbhb")
+fetch("http://127.0.0.1:3000/tannaz")
