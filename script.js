10ea6f807946bf084d25654c5b208e169b67e727
tannazma <97254921+tannazma@users.noreply.github.com>
2023-01-05 16:55:18 +0100

    added messageFromYou in js + beautifulize in css

--- script.js
+++ script.js
@@ -2,10 +2,9 @@
 fetch("http://127.0.0.1:3000/ardjbhb")
 fetch("http://127.0.0.1:3000/tannaz")
 
-
+messageFromMe = () => {
 const newMessageDiv = document.createElement("div")
 newMessageDiv.classList.add("message1")
-// newMessageDiv.textContent = "Hello again!"
 
 const messageFromYou = document.createElement("div")
 messageFromYou.textContent="Hello again!"
@@ -15,3 +14,20 @@ newMessageDiv.prepend(messageFromYou);
 const messageDiv = document.querySelector(".chat-container")
 messageDiv.classList.add("chat-container")
 document.getElementById("chatContainer").appendChild(newMessageDiv)
+}
+messageFromYou = () => {
+    const newMessageDiv = document.createElement("div")
+    newMessageDiv.classList.add("message2")
+    
+    const messageFromYou = document.createElement("div")
+    messageFromYou.textContent="Hi there!"
+    messageFromYou.classList.add("m2")
+    newMessageDiv.prepend(messageFromYou);
+    
+    const messageDiv = document.querySelector(".chat-container")
+    messageDiv.classList.add("chat-container")
+    document.getElementById("chatContainer").appendChild(newMessageDiv)
+    }
+
+messageFromMe()
+messageFromYou()
