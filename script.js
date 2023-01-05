3ec66dcf88cff6dbe3acc722a26a0e60068d330d
tannazma <97254921+tannazma@users.noreply.github.com>
2023-01-05 12:51:07 +0100

    add message from server in js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-console.log("Hello")
+
 fetch ("http://127.0.0.1:3000/ardjbhb")
 fetch("http://127.0.0.1:3000/tannaz")
+
+
+const newMessageDiv = document.createElement ("div");
+newMessageDiv.textContent="Hello again!";
+newMessageDiv.classList.add("message1", "m1")
+
+const messageFromYou=document.createElement("div")
+
+newMessageDiv.prepend(messageFromYou);
+document.body.appendChild(newMessageDiv)
