e576a3055fc9da5017de17bf83bfe0db53d97450
tannazma <97254921+tannazma@users.noreply.github.com>
2023-01-05 23:04:25 +0100

    refactor 2 func for messages  by 1 func with text

--- script.js
+++ script.js
@@ -2,32 +2,20 @@
 fetch("http://127.0.0.1:3000/ardjbhb")
 fetch("http://127.0.0.1:3000/tannaz")
 
-messageFromMe = () => {
-const newMessageDiv = document.createElement("div")
-newMessageDiv.classList.add("message1")
-
-const messageFromYou = document.createElement("div")
-messageFromYou.textContent="Hello again!"
-messageFromYou.classList.add("m1")
-newMessageDiv.prepend(messageFromYou);
-
-const messageDiv = document.querySelector(".chat-container")
-messageDiv.classList.add("chat-container")
-document.getElementById("chatContainer").appendChild(newMessageDiv)
-}
-messageFromYou = () => {
+messageFromAnyone = (text, align) => {
     const newMessageDiv = document.createElement("div")
     newMessageDiv.classList.add("message2")
-    
     const messageFromYou = document.createElement("div")
-    messageFromYou.textContent="Hi there!"
+    messageFromYou.textContent = text
     messageFromYou.classList.add("m2")
+
     newMessageDiv.prepend(messageFromYou);
-    
     const messageDiv = document.querySelector(".chat-container")
     messageDiv.classList.add("chat-container")
     document.getElementById("chatContainer").appendChild(newMessageDiv)
-    }
+}
 
-messageFromMe()
-messageFromYou()
+messageFromAnyone("Ok, let's go on 7 PM.")
+messageFromAnyone("Ok, let's go on 7 PM.")
+messageFromAnyone("Ok, let's go on 7 PM.")
+messageFromAnyone("Ok, let's go on 7 PM.")
